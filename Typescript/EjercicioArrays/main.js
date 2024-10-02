269fb41e7af071d0a8830d3843d64fbe9c7f4bf3
Xalgemn <116720398+MiquelAngelMartos@users.noreply.github.com>
2024-10-02 07:40:05 +0200

    ejercicios

--- Typescript/EjercicioArrays/main.js
+++ Typescript/EjercicioArrays/main.js
@@ -1,8 +1,49 @@
-var thisArray = ["[", "]"];
+var thisArray = [];
+var buttonPush = document.getElementById("push");
+var buttonUnshift = document.getElementById("unshift");
+var buttonInsert = document.getElementById("insert");
+var buttonPop = document.getElementById("pop");
+var buttonShift = document.getElementById("shift");
+var buttonRemove = document.getElementById("remove");
+var insertSelect = document.getElementById("insertIndex");
+var removeSelect = document.getElementById("removeIndex");
+function arrayPush() {
+    thisArray.push("🎂");
+    actualizarH1(thisArray.toString());
+}
+function arrayUnshift() {
+    thisArray.unshift("👀");
+    actualizarH1(thisArray.toString());
+}
+function arrayInsert() {
+    var selectedIndex = insertSelect.value;
+    var index = parseInt(selectedIndex);
+    thisArray.splice(index, 0, "🎁");
+    actualizarH1(thisArray.toString());
+}
+function arrayPop() {
+    thisArray.pop();
+    actualizarH1(thisArray.toString());
+}
+function arrayShift() {
+    thisArray.shift();
+    actualizarH1(thisArray.toString());
+}
+function arrayRemove() {
+    var removeIndex = removeSelect.value;
+    var index = parseInt(removeIndex);
+    thisArray.splice(index, 1);
+    actualizarH1(thisArray.toString());
+}
 function actualizarH1(texto) {
     var h1 = document.getElementById("arrayText");
     if (h1) {
         h1.textContent = texto;
     }
 }
-actualizarH1(thisArray.toString());
+buttonPush.addEventListener("click", arrayPush);
+buttonUnshift.addEventListener("click", arrayUnshift);
+buttonInsert.addEventListener("click", arrayInsert);
+buttonPop.addEventListener("click", arrayPop);
+buttonShift.addEventListener("click", arrayShift);
+buttonRemove.addEventListener("click", arrayRemove);
--- Typescript/EjercicioArrays/main.ts
+++ Typescript/EjercicioArrays/main.ts
@@ -1,11 +1,64 @@
 const thisArray: string[] = [];
 
-function actualizarH1(texto: string){
-    const h1 = document.getElementById("arrayText") as HTMLHeadingElement;
-    if (h1)
-    {
+const buttonPush = document.getElementById("push") as HTMLButtonElement;
+const buttonUnshift = document.getElementById("unshift") as HTMLButtonElement;
+const buttonInsert = document.getElementById("insert") as HTMLButtonElement;
+const buttonPop = document.getElementById("pop") as HTMLButtonElement;
+const buttonShift = document.getElementById("shift") as HTMLButtonElement;
+const buttonRemove = document.getElementById("remove") as HTMLButtonElement;
+
+const insertSelect = document.getElementById("insertIndex") as HTMLSelectElement;
+const removeSelect = document.getElementById("removeIndex") as HTMLSelectElement;
+
+function arrayPush() {
+    thisArray.push("🎂");
+    actualizarH1(thisArray.toString());
+}
+
+function arrayUnshift(){
+    thisArray.unshift("👀");
+    actualizarH1(thisArray.toString());
+}
+
+function arrayInsert(){
+    const selectedIndex = insertSelect.value;
+    const index = parseInt(selectedIndex);
+
+
+    thisArray.splice(index, 0,"🎁");
+    actualizarH1(thisArray.toString());
+}
+
+function arrayPop() {
+    thisArray.pop();
+    actualizarH1(thisArray.toString());
+}
+
+function arrayShift() {
+    thisArray.shift();
+    actualizarH1(thisArray.toString());
+}
+
+function arrayRemove(){
+    const removeIndex = removeSelect.value;
+    const index = parseInt(removeIndex);
+
+    thisArray.splice(index, 1);
+    actualizarH1(thisArray.toString());
+}
+
+function actualizarH1(texto: string) {
+    const h1 = document.getElementById("arrayText");
+    if (h1) {
         h1.textContent = texto;
     }
 }
 
-actualizarH1(thisArray.toString());
+
+
+buttonPush.addEventListener("click", arrayPush);
+buttonUnshift.addEventListener("click", arrayUnshift);
+buttonInsert.addEventListener("click", arrayInsert);
+buttonPop.addEventListener("click", arrayPop);
+buttonShift.addEventListener("click", arrayShift);
+buttonRemove.addEventListener("click",arrayRemove);
